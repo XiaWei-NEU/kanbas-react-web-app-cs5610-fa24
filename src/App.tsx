1c2f76e705eea81c02942670f5d93abb0db7bd42
XiaWei-NEU <xia.wei1@northeastern.edu>
2024-09-18 15:32:32 -0700

    a1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
 import Labs from './Labs';
+import Kanbas from './Kanbas';
+import { HashRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to Web Dev</h1>
-      <Labs />
-    </div>
+
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Kanbas"/>}/> 
+          <Route path="/labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
+        </Routes>
+      </div>
+    </HashRouter>
+
   );
 }
 
---
+++ src/Kanbas/Courses/Home/index.tsx
@@ -0,0 +1,31 @@
+import Modules from "../Modules";
+import CourseStatus from "./Status";
+export default function Home() {
+    return (
+        <div>
+            <div>
+                <button id="wd-add-assignment-group">Collapse All</button>
+                <button id="wd-add-assignment-group">View Progress</button>
+                <select id="wd-select-one-publish">
+                    <option selected value="PUBLISHALL">Publish All</option>
+                    <option value="PUBLISH1">Publish 1</option>
+                    <option value="PUBLISH2">Publish 2</option>
+                    <option value="PUBLISH3">Publish 3</option>
+                </select>
+                <button id="wd-add-assignment">+ Modul</button>
+            </div>
+            <table id="wd-home">
+                <tbody>
+                    <tr>
+                        <td valign="top">
+                            <Modules />
+                        </td>
+                        <td valign="top">
+                            <CourseStatus />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
