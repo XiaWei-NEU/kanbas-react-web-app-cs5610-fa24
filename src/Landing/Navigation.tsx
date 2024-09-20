c526f20febccc2f83a64b749b92617eb6ae5b575
XiaWei-NEU <xia.wei1@northeastern.edu>
2024-09-19 20:03:37 -0700

    a1

--- src/Landing/Navigation.tsx
+++ src/Landing/Navigation.tsx
@@ -2,9 +2,15 @@ import { Link } from "react-router-dom";
 export default function LandingNavigation() {
     return (
         <div id="wd-landing-navigation">
-            <Link to="../Kanbas" id="wd-kanbas-link">Kanbas</Link><br />
+            <h2>Lab Assignments</h2>
             <Link to="../Labs" id="wd-labs-link">Labs</Link><br />
-            <a href="https://github.com/XiaWei-NEU/kanbas-react-web-app-cs5610-fa24" id="wd-home-link">Source Code</a>
+            <Link to="../Labs/Lab1" id="wd-labs-link">Lab 1</Link><br />
+            <Link to="../Labs/Lab2" id="wd-labs-link">Lab 2</Link><br />
+            <Link to="../Labs/Lab3" id="wd-labs-link">Lab 3</Link><br />
+            <h2>Kanbas Application</h2>
+            <Link to="../Kanbas" id="wd-kanbas-link">Kanbas</Link><br />
+            <h2>Source Code Repositories</h2>
+            <a href="https://github.com/XiaWei-NEU/kanbas-react-web-app-cs5610-fa24" id="wd-home-link">A1 Repo</a>
         </div>
     );
 }
